d6ec761c2ad7dbe6512dfe14b459eed8d15ba3a9
chandana-codes <84772049+chandana-codes@users.noreply.github.com>
2023-12-10 15:55:35 +0530

    assignment - 1

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
-// console.log(heading);
-
-// React Functional Component
-const HeadingComponent = () => {
-  return <h1>Namaste React Functional Component</h1>;
-};
-
-//React Element
-const heading = (
+const Title = () => (
   <h1 className="head" tabIndex="5">
     Namaste React Using JSX
   </h1>
+);
+
+const number = 1000;
+
+// React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
