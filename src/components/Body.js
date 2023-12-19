9d63eda54ec539ac570a7f52b100795166ca5893
chandana-codes <84772049+chandana-codes@users.noreply.github.com>
2023-12-19 19:49:59 +0530

    Added Shimmer UI, Used useEffect, Fetching API data from the Swiggy

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
 
 export default Body = () => {
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+    const restaurants =
+      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants ?? [];
+    setListOfRestaurant(restaurants);
+  };
+
+  //Conditional Rendering
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
